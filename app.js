ace6ef64a29ce556906eecd49e22b204ee34f7ad
tomkeee <tomasz6612@gmail.com>
2021-04-07 19:57:13 +0200

    finish 1-stage

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -12,31 +13,39 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-var posts=[];
+let posts=[];
 
-app.get("/",function(req,res){
-res.render("home", {text:homeStartingContent, entries:posts});
-
+app.get("/",function(req,res)
+{
+ res.render("home", 
+ {
+  text:homeStartingContent,
+  entry:posts 
+ });
 });
 
-app.get("/about",function(req,res){
+app.get("/about",function(req,res)
+{
   res.render("about",{text:aboutContent});
 });
 
-app.get("/contact",function(req,res){
+app.get("/contact",function(req,res)
+{
   res.render("contact",{text:contactContent});
 });
 
-app.get("/compose",function(req,res){
+app.get("/compose",function(req,res)
+{
   res.render("compose")
 });
 
-app.post("/compose",function(req,res){
-  const post ={
+app.post("/compose",function(req,res)
+{
+  const post =
+  {
     Title : req.body.postTitle,
     Body : req.body.postBody
   };
@@ -44,7 +53,28 @@ app.post("/compose",function(req,res){
   res.redirect("/");
 });
 
+app.get("/posts/:postName",function(req,res)
+{
+  const requestedTitle=_.lowerCase(req.params.postName);
 
-app.listen(3000, function() {
+  posts.forEach(function(post)
+  {
+
+  const storedTitle =_.lowerCase(post.Title);
+
+    if (storedTitle===requestedTitle)
+    {
+      res.render("post",
+      {
+        title: post.Title,
+        body: post.Body
+      });
+    };
+  })
+});
+
+
+app.listen(3000, function() 
+{
   console.log("Server started on port 3000");
 });
